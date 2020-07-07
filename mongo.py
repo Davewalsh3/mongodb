f825c7125322a5f720ad76a2bf58917e552b4cd0
DavW <Davidpmwalsh@protonmail.com>
2020-07-07 16:41:28 +0000

    inital commit

--- mongo.py
+++ mongo.py
@@ -2,12 +2,13 @@ import pymongo
 import os
 from os import path
 if path.exists("env.py"):
-  import env 
+    import env
 
 
-MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "mytestdb"
+MONGODB_URI = os.environ.get("MONGODB_URIs")
+DBS_NAME = "mytestdatabase"
 COLLECTION_NAME = "myfirstmdb"
+
 
 def mongo_connect(url):
     try:
@@ -17,15 +18,17 @@ def mongo_connect(url):
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
         
+
 conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_doc = {"first":"douglas","last":"adams","dob":"11/03/1990","hair_colour":"gray","occupation":"writer","nationality":"english"}
-
-coll.insert_one(new_doc)
+new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'hair_colour':
+ 'grey', 'occupation': 'writer', 'nationality': 'english'}
 
 documents = coll.find()
 
 for doc in documents:
     print(doc)
+
+    
