ecf698e02f5e1418f74f8e8fb2ca25e4a5c3a81e
DavW <Davidpmwalsh@protonmail.com>
2020-07-09 14:56:51 +0000

    added deleting document

--- mongo_projects.py
+++ mongo_projects.py
@@ -105,6 +105,28 @@ def edit_record():
         print("error accessing the database")
 
 
+def delete_record():
+
+    doc = get_record()
+
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+        
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Document not deleted")
+
+
 def main_loop():  # 1 main loop wil cntnue to cll our mnu evrytme we cm bk 2it
     while True:
         option = show_menu()
@@ -115,7 +137,7 @@ def main_loop():  # 1 main loop wil cntnue to cll our mnu evrytme we cm bk 2it
         elif option == "3":
             edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":  # 1 if we slct 5, close cnction(break)
             conn.close()
             break
